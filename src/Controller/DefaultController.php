3738f304afd95c3becaae68b934e95b7e5c89fe4
Jeff <falecomjeff@gmail.com>
2018-04-04 17:10:05 -0300

    Create code for search (controller, formType and view), install bootstrap front-end framework and completed the basic search.

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -16,7 +16,8 @@ class DefaultController extends Controller
      */
     public function index(Request $request)
     {
-        $form = $this->createForm(DefaultType::class);
+        $form   = $this->createForm(DefaultType::class);
+        $results = null;
 
         $form->handleRequest($request);
 
@@ -24,36 +25,13 @@ class DefaultController extends Controller
             $githubClient     = new Client();
             $searchParameters = $form->getData();
 
-            switch ($searchParameters["type"]) {
-                case 'repositorios':
-                    $result = $githubClient->api('search')->repositories($searchParameters['name']);
-                    break;
-
-                case 'codigos':
-                    $result = $githubClient->api('search')->code($searchParameters['name']);
-                    break;
-
-                case 'issues':
-                    $result = $githubClient->api('search')->issues($searchParameters['name']);
-                    break;
-
-                case 'usuarios':
-                    $result = $githubClient->api('search')->users($searchParameters['name']);
-                    break;
-
-                case 'commits':
-                    $result = $githubClient->api('search')->commits($searchParameters['name']);
-                    break;
-            }
-
-            echo "<pre>";
-            var_dump($result);
-            die();
+            $results = $githubClient->api('search')->repositories($searchParameters['name']);
         }
 
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
             'form'            => $form->createView(),
+            'results'         => $results,
         ]);
     }
 }
--- src/Form/DefaultType.php
+++ src/Form/DefaultType.php
@@ -6,6 +6,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
@@ -14,18 +15,16 @@ class DefaultType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('type', ChoiceType::class, array(
-                'choices'  => array(
-                    'Repositórios' => 'repositorios',
-                    'Códigos'      => 'codigos',
-                    'Issues'       => 'issues',
-                    'Usuários'     => 'usuarios',
-                    'Commits'      => 'commits',
+            ->add('name', TextType::class, array(
+                'attr'   => array(
+                    'class' => 'form-control'
                 ),
-                'placeholder'  => 'Pesquisar por...',
             ))
-            ->add('save', SubmitType::class)
+            ->add('save', SubmitType::class, array(
+                'attr'   => array(
+                    'class' => 'btn btn-primary'
+                ),
+            ))
         ;
     }
 
